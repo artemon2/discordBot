1e5723cc52cbb4b084092a446930dc0f260d481d
artemon2 <90180345+artemon2@users.noreply.github.com>
2023-08-24 12:08:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,31 @@ YDL_OPTIONS = {'format': '140', 'noplaylist': 'False', 'simulate': 'True', 'key'
 
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
 
+queues = {}
+
 intents = discord.Intents.all()
 intents.message_content = True
 intents.typing = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+def check_queue(guild_id):
+    if queues[guild_id] != []:
+        voice = bot.get_guild(guild_id).voice_client
+        song = queues[guild_id].pop(0)
+
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            if 'https://youtu.be' or 'http://youtube.com' in song:
+                info = ydl.extract_info(song, download=False)
+            else:
+                info = ydl.extract_info(f"ytsearch:{song}", download=False)['entries'][0]
+        link = info['formats'][0]['url']
+        source = discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=link,**FFMPEG_OPTIONS)  # Создаем новый source
+
+        voice.play(source, after=lambda x=None: check_queue(guild_id))  # Используйте только guild_id
+
+
 
 @bot.event
 async def on_ready():
@@ -30,26 +49,53 @@ async def test(ctx, *arg):
 async def hello(ctx):
     await ctx.send("Пошел нахуй, " + str(ctx.author.mention))
 
+# @bot.command(pass_context = True)
+# async def play(ctx, url):
+#
+#     if(ctx.author.voice):
+#         if ctx.guild.voice_client:
+#              await ctx.guild.voice_client.move_to(ctx.author.voice.channel)
+#
+#         else:
+#             vc = await ctx.author.voice.channel.connect()
+#
+#         with YoutubeDL(YDL_OPTIONS) as ydl:
+#             if 'https://' in url:
+#                 info = ydl.extract_info(url, download=False)
+#             else:
+#                 info = ydl.extract_info(f"ytsearch:{url}", download=False)['entries'][0]
+#         link = info['formats'][0]['url']
+#
+#         source = discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=link, **FFMPEG_OPTIONS)
+#
+#         vc.play(source, after=lambda x=None: check_queue(ctx.guild.id))  # Передаем объект source
+#     else:
+#         await ctx.send("Ви не в голосовому каналі, ви повинні "
+#                        "зайти в голосовий канал, щоб використати цю команду!")
 @bot.command(pass_context = True)
 async def play(ctx, url):
+    if (ctx.author.voice):
 
-    if(ctx.author.voice):
-        if ctx.guild.voice_client:
-            await ctx.guild.voice_client.move_to(ctx.author.voice.channel)
+        if ctx.guild.voice_client is None:
+            await ctx.author.voice.channel.connect()
 
+        song = url
+        guild_id = ctx.message.guild.id
+
+        if guild_id in queues:
+            queues[guild_id].append(song)
         else:
-            vc = await ctx.author.voice.channel.connect()
+            queues[guild_id] = [song]
 
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            if 'https://' in url:
-                info = ydl.extract_info(url, download=False)
-            else:
-                info = ydl.extract_info(f"ytsearch:{url}", download=False)['entries'][0]
-        link = info['formats'][0]['url']
-        vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=link, **FFMPEG_OPTIONS))
-    else:
-        await ctx.send("Ви не в голосовому каналі, ви повинні "
-                       "зайти в голосовий канал, щоб використати цю команду!")
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        if len(queues) == 1 and not voice.is_playing():
+            check_queue(guild_id)
+        else: await ctx.send("Додано до черги.")
+
+    else: await ctx.send("Ви не в голосовому каналі, ви повинні зайти в голосовий канал, щоб використати цю команду!")
+
+
+
 @bot.command(pass_context = True)
 async def pause(ctx):
     voice = discord.utils.get(bot.voice_clients,guild=ctx.guild)
